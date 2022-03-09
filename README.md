ea6586441a51e7e0b0eba3651573d8aa224df503
Battle Chicken <felix@larkov.de>
2022-03-09 07:02:52 -0500

    fixed a syntax error in insall.sh

--- README.md
+++ README.md
@@ -2,8 +2,8 @@ These are my config files across my various machines.
 
 
 
-WM: Bspwm
-Launcher: Rofi
+WM: Bspwm 
+Launcher: Rofi 
 Bar: Polybar
 Editor: Neovim
 Shell:Zsh
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-home_files=( .bashrc .zshrc .xinitrc .gitconfig )
+home_files=( .bashrc .zshrc .xinitrc .gitconfig .aliases)
 echo $home_files
 config_files=( nvim sxhkd bspwm alacritty starship rofi polybar )
 echo "----- Installing Dotfiles -----"
@@ -13,23 +13,24 @@ fi
 #create the .extra file if it doesn't exist
 if test -f ~/.extra/; then
   echo ".extra exists"
-else mkdir ~/.extra && echo "creating .extra file"
+else touch ~/.extra && echo "creating .extra file"
 fi
 
 
 #create symlinks for each file in the home folder
-for i in "{$home_files[@]}"
+for i in "${home_files[@]}"
 do
   if test -f ~/$i; then
-    echo "---$i already exists or is already linked"
+    echo "$i already exists or is already linked"
   else ln -s ~/dotfiles/$i ~/ && echo "linking $i"
   fi
+done
 
 #create symlinks for each file in the .config folder
 for i in "${config_files[@]}"
 do
   if test -d ~/.config/$i;then
-    echo "---$i already exists or is already linked---"
-  else ln -s ~/dotfiles/.config/"$i" ~/.config
+    echo "$i already exists or is already linked"
+  else ln -s ~/dotfiles/.config/"$i" ~/.config/ && echo "linking $i"
   fi
 done
