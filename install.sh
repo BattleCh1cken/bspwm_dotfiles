6ac6615b3a1c08b5a90461863bd75444254c7e88
Battle Chicken <felix@larkov.de>
2022-03-12 14:37:43 -0500

    tweaked bspwm

--- install.sh
+++ install.sh
@@ -1,35 +1,9 @@
 #!/bin/bash
-home_files=( .bashrc .zshrc .xinitrc .gitconfig .aliases)
-config_files=( nvim sxhkd bspwm alacritty starship rofi polybar neofetch ranger kitty picom)
-echo "----- Installing Dotfiles -----"
-
-#create the .env_variables file
-if test -f ~/.env_variables; then
-  echo "env_variables file exists"
-else touch ~/.env_variables && echo "creating .env_variables file"
-fi
-
-#create the .extra file if it doesn't exist
-if test -f ~/.extra/; then
-  echo ".extra exists"
-else touch ~/.extra && echo "creating .extra file"
-fi
 
 
-#create symlinks for each file in the home folder
-for i in "${home_files[@]}"
+for i in "${pacman_pkgs[@]}"
 do
-  if test -f ~/$i; then
-    echo "$i already exists or is already linked"
-  else ln -s ~/dotfiles/$i ~/ && echo "linking $i"
-  fi
+  echo "$i"
 done
 
-#create symlinks for each file in the .config folder
-for i in "${config_files[@]}"
-do
-  if test -d ~/.config/$i;then
-    echo "$i already exists or is already linked"
-  else ln -s ~/dotfiles/.config/"$i" ~/.config/ && echo "linking $i"
-  fi
-done
+
