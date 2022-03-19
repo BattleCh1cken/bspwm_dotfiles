ce8661e1262d2c774f00253ece6f7f763a0a46f4
Battle Chicken <felix@larkov.de>
2022-03-19 12:43:55 -0400

    'borrowed' a cool polybar from archcraft

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,8 @@
-#!/usr/bin/env bash
+	# Terminate already running bar instances
+  killall -q polybar
 
-for i in /sys/class/hwmon/hwmon*/temp*_input; do
-	if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "k10temp: Tctl" ]; then
-		export HWMON_PATH="$i"
-	fi
-done
-export DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
-killall -q polybar
-echo "---" | tee -a /tmp/polybar1.log
-polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Bar launched"
+	# Launch the barpolybar
+polybar -q main -c ~/.config/polybar/config.ini &
